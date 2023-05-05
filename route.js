8c3a202007fddbbd53f818f18051c3bdf8767ba2
Kira2907 <130835235+Kira2907@users.noreply.github.com>
2023-05-05 12:57:51 +0530

    Wrap
    Divided the 'write and read' code into 2 separate files.

---
+++ route.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+
+const requestHandler= (req, res) => {
+    const url = req.url;
+    const method = req.method; 
+    
+    if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My first project</title></head>");
+    res.write("<body>");
+
+    // read messages from file
+    const messages = fs.readFileSync("message.txt", "utf-8").split("\n");
+
+    res.write("<h1>Messages:</h1><ul>");
+    messages.forEach((msg) => {
+        if (msg !== "") {
+          res.write(`<li>${msg}</li>`);
+        }
+      });
+    res.write("</ul>");
+
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+
+      // append message to file
+      fs.appendFileSync("message.txt", message + "\n");
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Node.js response</title></head>");
+  res.write("<body><h1>Welcome to my Node.js project</h1></body>");
+  res.write("</html>");
+  return res.end();
+}; 
+  
+//module.exports=requestHandler;
+
+module.exports={
+    handler: requestHandler,
+    text: 'some text' 
+}
+
+//exports.handler= requestHandler;
+ //exports.text= 'some text';
