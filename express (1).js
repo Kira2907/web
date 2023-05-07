cf213c80e967ba1b61ac5499e464582cde34fbe9
Kira2907 <130835235+Kira2907@users.noreply.github.com>
2023-05-07 13:42:35 +0530

    Middleware
    Created an /add -product route using middleware and parsed the request body using body -parser and printed it in console. Also redirected to '/' after clicking submit

---
+++ express (1).js
@@ -0,0 +1,22 @@
+const express= require("express");
+const bodyParser= require('body-parser');
+
+const app= express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="text" name="size"> <button type="submit">ADD PRODUCT</form> ');
+});
+
+app.post('/product', (req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req,res,next) => {
+    console.log('in another middleware'); 
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(4000);
